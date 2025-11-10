8e203fd6ed0a3cdc836b36887778eee30c2b6d46
muhlenlogan8 <muhlenlogan8@gmail.com>
2025-11-10 17:58:33 -0500

    removing dev socket and putting prod socket connection

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Initialize and export the socket connection
-// export const socket = io("/", { transports: ["websocket", "polling"] });
+export const socket = io("/", { transports: ["websocket", "polling"] });
 
 // Dev stuff
-export const socket = io("http://localhost:8000", { autoConnect: true });
+// export const socket = io("http://localhost:8000", { autoConnect: true });
